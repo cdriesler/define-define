3ba2eba12cf55321cacac2df4ee31af30a678a5e
chuck <cdriesler.iv@gmail.com>
2019-08-25 17:19:01 -0400

    testing offset curve

---
+++ api/src/compute.geometry/Predicates/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Extensions;
+using Newtonsoft.Json;
+using Rhino.Geometry;
+
+namespace compute.geometry
+{
+    public class Test : NancyModule
+    {
+        public Test(Nancy.Routing.IRouteCacheProvider routeCacheProvider)
+        {
+            Get["/test"] = _ => Something(Context);
+        }
+
+        static Response Something(NancyContext Context)
+        {
+            var fakeCurve = new LineCurve(Point3d.Origin, new Point3d(0, 1, 0));
+
+            var receivedCurve = JsonConvert.DeserializeObject<Curve>(Context.Request.Body.AsString());
+
+            var offsetCurve = fakeCurve.Offset(Plane.WorldXY, 5, 0.1, CurveOffsetCornerStyle.None)[0];
+
+            return JsonConvert.SerializeObject(offsetCurve);
+        }
+
+    }
+}
