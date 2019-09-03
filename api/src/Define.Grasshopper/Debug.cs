61e1be4c758ec13c2d9ae1a9666d07f817589289
atabbakh1 <54362791+atabbakh1@users.noreply.github.com>
2019-09-02 20:09:44 -0400

    Calcuate the distance between the outer edges

--- api/src/Define.Grasshopper/Debug.cs
+++ api/src/Define.Grasshopper/Debug.cs
@@ -6,76 +6,90 @@ using System.Text;
 using System.Threading.Tasks;
 using Rhino;
 using Grasshopper.Kernel;
+using Rhino.Geometry;
 
 namespace Define.Grasshopper
 {
-    public class CurveSlope : GH_Component
+    public class SideEdgesDistance : GH_Component
     {
-        public CurveSlope() : base("Curve Slope", "CSlope", "Returns slope of 2D curve.", "Curve", "Analysis")
+        public SideEdgesDistance() : base("Distance", "EDist", "Calculates the distance between the outer edges.", "Curve", "Util")
         {
 
         }
 
         protected override void RegisterInputParams(GH_InputParamManager pManager)
+
+        // inputs 
         {
-            pManager.AddCurveParameter("Curve", "C", "Curve to measure.", GH_ParamAccess.item);
+            pManager.AddCurveParameter("Left Edge", "L", "Left Curve Test", GH_ParamAccess.item);
+            pManager.AddCurveParameter("Right Edge", "R", "Right Curve Test", GH_ParamAccess.item);
+            pManager.AddCurveParameter("canvas boundary", "B", "Canvas Boundary", GH_ParamAccess.item);
+
         }
 
+        //Outputs
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("Slope", "d", "Slope of curve.", GH_ParamAccess.item);
-            pManager.AddIntegerParameter("Proportion", "P", "General proportion. 0 = Horizontal (dY = 0) 1 = Landscape (dX > dY) 2 = Sqaure (dX = dY) 3 = Portrait (dY > dX) 4 = Vertical (dX = 0)", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Distance Between Edges", "D", "Center To Center Measure", GH_ParamAccess.item);
+            pManager.AddCurveParameter("Canvas", "B", "New Canvas", GH_ParamAccess.item);
         }
 
+
+        //inputs reference
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            Rhino.Geometry.Curve curveToMeasure = null;
-            DA.GetData(0, ref curveToMeasure);
 
-            try
+            Curve LeftEdge = null;
+            DA.GetData(0, ref LeftEdge);
+
+            Curve RightEdge = null;
+            DA.GetData(1, ref RightEdge);
+
+            Curve canvasBoundary = null;
+            DA.GetData(2, ref canvasBoundary);
+
+
+
+
+
+            //Logics
+        
+            double Distance = 0;
+
+            double LeftLength = LeftEdge.GetLength();
+            double RightLength = RightEdge.GetLength();
+
+            Point3d LeftMidPoint = LeftEdge.PointAtLength(LeftLength / 2);
+            Point3d RightMidPoint = RightEdge.PointAtLength(RightLength / 2);
+
+            Line DistanceProjection = new Line(LeftMidPoint, RightMidPoint);
+
+            Distance = DistanceProjection.ToNurbsCurve().GetLength();
+
+
+
+            // Canvas Boundary Test
+            var area = AreaMassProperties.Compute(canvasBoundary).Area;
+
+            if (area != 864)
             {
-                curveToMeasure.GetLength();
+                int width = 36;
+                int height = 24;
+               
+                canvasBoundary = new Rectangle3d(Plane.WorldXY, width, height).ToNurbsCurve();
             }
-            catch (Exception e)
+
+            else if( area == 864)
             {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, e.ToString());
-                return;
+                Point3d CanvasCenter = canvasBoundary.GetBoundingBox(true).Center;
+                var Vector = new Vector3d(CanvasCenter.X, CanvasCenter.Y, CanvasCenter.Z) * (-1);
+                canvasBoundary.Transform(Transform.Translation(Vector));       
             }
 
-            Rhino.Geometry.Point3d startPoint = curveToMeasure.PointAtStart;
-            Rhino.Geometry.Point3d endPoint = curveToMeasure.PointAtEnd;
-
-            double dY = endPoint.Y - startPoint.Y;
-            double dX = endPoint.X - startPoint.X;
-
-            if (dX == 0)
-            {
-                DA.SetData(0, null);
-                DA.SetData(1, 4);
-            }
-            else
-            {
-                double slope = dY / dX;
-
-                DA.SetData(0, slope);
-
-                if (dY == 0)
-                {
-                    DA.SetData(1, 0);
-                }
-                else if (dX > dY)
-                {
-                    DA.SetData(1, 1);
-                }
-                else if (dX == dY)
-                {
-                    DA.SetData(1, 2);
-                }
-                else if (dY > dX)
-                {
-                    DA.SetData(1, 3);
-                }
-            }
+            // Assign Output
+            DA.SetData(0, Distance);
+            DA.SetData(1, canvasBoundary);
+            
         }
 
         protected override System.Drawing.Bitmap Icon
@@ -86,7 +100,7 @@ namespace Define.Grasshopper
 
         public override Guid ComponentGuid
         {
-            get { return new Guid("abfaff44-daaf-43b1-a173-2063e5d5864c"); }
+            get { return new Guid("f05e9e11-1660-4342-9c16-31344fdca591"); }
         }
     }
 }
--- api/src/compute.geometry/Models/DrawingMaster.cs
+++ api/src/compute.geometry/Models/DrawingMaster.cs
@@ -118,6 +118,8 @@ namespace compute.geometry
         }
 
 
+  
+
 
     }
 }
