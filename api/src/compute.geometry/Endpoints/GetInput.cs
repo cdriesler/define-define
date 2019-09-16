5e5f249359e3a75aedc4959c012df24fc90cecba
chuck <cdriesler.iv@gmail.com>
2019-09-16 02:14:39 -0400

    add all measurement routines

--- api/src/compute.geometry/Endpoints/GetInput.cs
+++ api/src/compute.geometry/Endpoints/GetInput.cs
@@ -60,8 +60,18 @@ namespace Define.Api
                     return res;
                 }
 
-                var measurement = translator(lines);
+                var measurement = -1.0;
 
+                try
+                {
+                    measurement = translator(lines);
+                }
+                catch (Exception e)
+                {
+                    // Do nothing
+                    Console.WriteLine(e.Message);
+                }
+                
                 if (measurement < 0)
                 {
                     measurement = 0;
@@ -135,29 +145,122 @@ namespace Define.Api
             return (distanceA + distanceB) / 2;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="crvs"></param>
+        /// <returns></returns>
         public static double OnOpenings(List<Polyline> crvs)
         {
-            return 0.25;
+            if (crvs.Count <= 0)
+            {
+                return -1;
+            }
+            else if (crvs.Count == 1)
+            {
+                var c = crvs[0].ToNurbsCurve();
+
+                var mid = c.PointAtNormalizedLength(0.5);
+
+                var start = c.PointAtStart - mid;
+                var end = c.PointAtEnd - mid;
+
+                var angle = Vector3d.VectorAngle(new Vector3d(start), new Vector3d(end), Plane.WorldXY);
+                var deg = (angle * (180 / Math.PI)) % 90;
+
+                return deg / 90;
+            }
+            else if (crvs.Count >= 2)
+            {
+                var cA = crvs[0].ToNurbsCurve();
+                var cB = crvs[crvs.Count - 1].ToNurbsCurve();
+
+                var cApts = new List<Point3d>() { cA.PointAtStart, cA.PointAtEnd }.OrderBy(x => x.Y);
+                var cBpts = new List<Point3d>() { cB.PointAtStart, cB.PointAtEnd }.OrderBy(x => x.Y);
+
+                var angle = Vector3d.VectorAngle(new Vector3d(cApts.Last() - cApts.First()), new Vector3d(cBpts.Last() - cBpts.First()));
+                var deg = (angle * (180 / Math.PI)) % 90;
+
+                return deg / 90;
+            }
+
+            return -1;
         }
 
         public static double OnDisjoint(List<Polyline> crvs)
         {
-            return 0.35;
+            var angles = new List<double>();
+
+            crvs.ForEach(x =>
+            {
+                var c = x.ToNurbsCurve();
+                var angle = Vector3d.VectorAngle(Vector3d.XAxis, new Vector3d(c.PointAtEnd - c.PointAtStart));
+                angles.Add(Math.Abs((angle * (180 / Math.PI))) % 90);
+            });
+
+            return angles.Average() / 90;
         }
 
         public static double OnThreshold(List<Polyline> crvs)
         {
-            return 0.45;
+            if (crvs.Count == 0)
+            {
+                return 0.1;
+            }
+            else
+            {
+                var sizes = new List<double>();
+
+                crvs.ForEach(x =>
+                {
+                    sizes.Add(x.ToNurbsCurve().GetLength());
+                });
+
+                return sizes.Average() / 3;
+            }
         }
 
         public static double OnPorosity(List<Polyline> crvs)
         {
-            return 0.55;
+            if (crvs.Count < 2)
+            {
+                return -1;
+            }
+            else
+            {
+                var areas = new List<double>();
+
+                crvs.ForEach(x =>
+                {
+                    areas.Add(x.ToNurbsCurve().GetBoundingBox(false).Area);
+                });
+
+                areas.Sort();
+
+                return areas.First() / areas.Last();
+            }
         }
 
         public static double OnParallel(List<Polyline> crvs)
         {
-            return 0.75;
+            if (crvs.Count < 2)
+            {
+                return 1;
+            }
+            else 
+            {
+                var angles = new List<double>();
+
+                crvs.ForEach(x =>
+                {
+                    var c = x.ToNurbsCurve();
+                    angles.Add(Vector3d.VectorAngle(Vector3d.XAxis, new Vector3d(c.PointAtEnd - c.PointAtStart)));
+                });
+
+                angles.Sort();
+
+                return angles.First() / angles.Last();
+            }
         }
 
     }
